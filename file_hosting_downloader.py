c529768ae0449773271049c807162365f19edbcb
Omar <ramirez0000245@gmail.com>
2023-07-26 17:26:40 -0700

    update for discription and example

--- file_hosting_downloader.py
+++ file_hosting_downloader.py
@@ -2,24 +2,23 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-URL_LIST = [
-    'https://thdisc-my.sharepoint.com/:f:/g/personal/thdisc_v1_1_thdisc_tk/EnAnELsu6FVHrFFZGF1rjOkB2pcgog7tSTQ5rcmoBd7hfg?e=NZwHM4',
-    'https://thdisc-my.sharepoint.com/:f:/g/personal/thdisc_v1_1_thdisc_tk/Er8xdPUGs7JLtfDSqoV6vTgB7rRR3SYdDxhy6qQuogdF5Q?e=GXnQsn',
-
-    ]
+#Add your URL links to URL_LIST
+URL_LIST = ['https://thdisc-my.sharepoint.com/:f:/g/personal/example']
+#Add website substring to check for comparison  and xpath to click on to download
 FILE_HOSTING_LIST = [ 
         {'url_list': "mega.nz", 'xpath': '/html/body/div[6]/div[2]/div[2]/section/div[1]/div/div[4]/div[3]/div[2]/div[1]/button'},
         {'url_list': 'my.sharepoint', 'xpath': '/html/body/div[1]/div/div[2]/div/div/div/div[2]/div[1]/div/div/div/div/div/div[1]/div/button/span/span/span'} 
     ]
-
+#click by xpath
 def driverclick_byxpath(xpath, driver):
     driver.find_element(By.XPATH, xpath).click()
-
-def file_hosting_downloader(URL_LIST):
+    
+#import list of url and list of file hosting
+def file_hosting_downloader(URL_LIST, FILE_HOSTING_LIST):
     driver = webdriver.Chrome()
     driver.implicitly_wait(30)
 
-    if isinstance(URL_LIST, list):
+    if isinstance(URL_LIST, list) & isinstance(FILE_HOSTING_LIST, list):
         for url in URL_LIST:
             for item in FILE_HOSTING_LIST:
                 if item["url_list"] in url:
@@ -30,5 +29,5 @@ def file_hosting_downloader(URL_LIST):
 
 # __name__ special variables
 if __name__ == '__main__':
-    file_hosting_downloader(URL_LIST)
+    file_hosting_downloader(URL_LIST, FILE_HOSTING_LIST)
 
